abbf2194ac47d2cfd5f6ace85e8808dae2721c65
SmitHack2022 <117286898+SmitHack2022@users.noreply.github.com>
2022-11-02 19:59:45 +0530

    Add files via upload
    Web Scanner for no control Automation

---
+++ SearchElement.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+from selenium import webdriver
+import pyautogui
+import pytesseract
+import re
+from time import sleep
+
+
+chrome_path = 'D:\python_file\LearningProjects\chromedriver.exe'
+pytesseract.pytesseract.tesseract_cmd = 'D:\other\module\\tesseract.exe'
+
+
+def open_website(url):
+    driver = webdriver.Chrome(executable_path=chrome_path)
+    driver.maximize_window()
+    driver.get(url)
+    sleep(1)
+    screenshot = pyautogui.screenshot(region=(0, 150, 1920, 880))
+    driver.close()
+    return screenshot
+
+
+def display_and_save(screenshot, WEBSITE_URL):
+    image = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f"static/img/scrap_img/{WEBSITE_URL[12:12+5]}.png", image)
+    return image
+
+
+def image_processing(image):
+    # Convert the image to gray scale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+
+    # Kernel size increases or decreases the area of the rectangle to be detected.
+    # A smaller value like (10, 10) will detect each word instead of a sentence.
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
+
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    return contours
+
+
+def get_web_elements(detected_data, image, WEBSITE_URL):
+    element_set = set()
+    element_data = []
+    for element in detected_data:
+        # x coordinate, y cordinate, height and width of element
+        x, y, w, h = cv2.boundingRect(element)
+
+        # Cropping the text block for giving input to OCR
+        cropped_box = image[y:y + h, x:x + w]
+
+        # Apply OCR on the cropped image
+        box_data = pytesseract.image_to_string(cropped_box)
+
+        #basic text filteration
+        box_data = re.sub('\s+', '', box_data)
+
+        if box_data != "" and box_data not in element_set:
+            img = cv2.cvtColor(np.array(cropped_box), cv2.COLOR_RGB2BGR)
+            cv2.imwrite(f"static/img/scrap_img/{box_data}.png", img)
+
+            # Drawing a rectangle on copied image
+            rect = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            element_set.add(box_data)
+            element_data.append({
+                'img_url': f'img/scrap_img/{box_data}.png',
+                'name': box_data,
+                'x_cod': x,
+                'y_cod': y,
+                'height': h,
+                'width': w,
+                'website': WEBSITE_URL[12:17],
+                'main_name': box_data
+            })
+
+    rect = cv2.cvtColor(np.array(rect), cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f"static/img/scrap_img/{WEBSITE_URL[12:17]}_box.png", rect)
+    return element_data
+
+
+
+def get_website_data(url):
+    WEBSITE_URL = url
+    screenshot = open_website(WEBSITE_URL)
+    image = display_and_save(screenshot, WEBSITE_URL)
+    detected_regions = image_processing(image)
+    element_data = get_web_elements(detected_regions, image, WEBSITE_URL)
+    return element_data
+
+
+def custom_detection(element_data, url):
+    x_cod, y_cod, height, width = element_data.x_cod, element_data.y_cod, element_data.height, element_data.width
+    image = cv2.imread(f"static/img/scrap_img/{url}.png", cv2.IMREAD_COLOR)
+
+    cropped_box = image[y_cod:(y_cod + height), x_cod:(x_cod + width)]
+    new_crop_img = cv2.cvtColor(np.array(cropped_box), cv2.COLOR_RGB2BGR)
+    box_data = pytesseract.image_to_string(new_crop_img)
+
+    box_data = re.sub('\s+', '', box_data)
+
+    element_data.main_name = box_data
+
+    cv2.imwrite(f"static/img/scrap_img/{element_data.name}.png", new_crop_img)
+    element_data.img_url = f"img/scrap_img/{element_data.name}.png"
+
+    box_img = cv2.imread(f"static/img/scrap_img/{url}_box.png", cv2.IMREAD_COLOR)
+    rect = cv2.rectangle(box_img, (x_cod, y_cod), (x_cod + width, y_cod + height), (0, 255, 0), 2)
+    rect = cv2.cvtColor(np.array(rect), cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f"static/img/scrap_img/{url}_box.png", rect)
+
+    return element_data
+
+
+if __name__ == "__main__":
+    element_data = get_website_data('https://www.myntra.com/')
+    for data in element_data:
+        print(data['img_url'])
+        print(data['name'])
+        print(data['x_cod'])
+        print()
---
+++ app.py
@@ -0,0 +1,203 @@
+from flask import Flask, render_template, redirect, url_for, request
+from SearchElement import get_website_data, custom_detection
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from types import SimpleNamespace
+import json
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///web_element.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CONFIGURE TABLE
+class ElementTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    main_name = db.Column(db.String(250))
+    x_cod = db.Column(db.Integer)
+    y_cod = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+    width = db.Column(db.Integer)
+    img_url = db.Column(db.String(250))
+    website = db.Column(db.String(300))
+
+
+class WebsiteTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    web_url = db.Column(db.String(250))
+
+
+db.create_all()
+
+website_url = None
+
+
+@app.route('/')
+def home_page():
+    print("home website = ", website_url)
+    table_data = []
+    if website_url:
+        table_data = ElementTable.query.filter_by(website=website_url[12:17]).all()
+    return render_template("home.html", table_data=table_data, website_url=website_url)
+
+
+@app.route("/post/<int:element_id>")
+def show_element(element_id):
+    requested_element = ElementTable.query.get(element_id)
+    return render_template("element.html", element=requested_element)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/Team")
+def contact():
+    return render_template("team.html")
+
+
+@app.route("/", methods=["POST"])
+def get_website_url():
+    global website_url
+    website_url = request.form.get("website_url")
+    table_data = ElementTable.query.filter_by(website=website_url[12:12+5]).all()
+    if not table_data:
+        element_data = get_website_data(website_url)
+        for data in element_data:
+            insert_data = ElementTable(
+                name=data['name'],
+                main_name=data['main_name'],
+                x_cod=data['x_cod'],
+                y_cod=data['y_cod'],
+                height=data['height'],
+                width=data['width'],
+                img_url=data['img_url'],
+                website=website_url[12:12+5],
+            )
+            db.session.add(insert_data)
+            db.session.commit()
+            table_data = ElementTable.query.filter_by(website=website_url[12:12+5]).all()
+
+    return render_template("home.html", table_data=table_data, website_url=website_url)
+
+
+@app.route("/delete/<int:element_id>")
+def delete_element(element_id):
+    element_to_delete = ElementTable.query.get(element_id)
+    db.session.delete(element_to_delete)
+    db.session.commit()
+    print("del website = ", website_url)
+    return redirect(url_for('home_page'))
+
+
+@app.route("/edit_element/<int:element_id>", methods=["GET", "POST"])
+def edit_element(element_id):
+    element = ElementTable.query.get(element_id)
+
+    if request.method == 'POST':
+        element.name = str(request.form.get('name'))
+        element.width = int(request.form.get('width'))
+        element.height = int(request.form.get('height'))
+        element.x_cod = int(request.form.get('x_cod'))
+        element.y_cod = int(request.form.get('y_cod'))
+        element.main_name = str(request.form.get('main_name'))
+
+        element = custom_detection(element, element.website)
+        edit_element = ElementTable(
+            name=element.name,
+            main_name=element.main_name,
+            x_cod=element.x_cod,
+            y_cod=element.y_cod,
+            height=element.height,
+            width=element.width,
+            img_url=element.img_url,
+            website=element.website,
+        )
+        db.session.commit()
+        return redirect(url_for("show_element", element_id=element.id))
+    return render_template("edit_element.html", element=element, is_edit=True)
+
+
+@app.route("/new_element", methods=["GET", "POST"])
+def add_new_element():
+    if request.method == 'POST':
+        is_exit = ElementTable.query.filter_by(name=request.form.get('name')).all()
+        if is_exit:
+            return render_template("edit_element.html", element=is_exit, is_edit=False, present=True)
+
+        element = dict()
+        element['name'] = str(request.form.get('name'))
+        element['width'] = int(request.form.get('width'))
+        element['height'] = int(request.form.get('height'))
+        element['x_cod'] = int(request.form.get('x_cod'))
+        element['y_cod'] = int(request.form.get('y_cod'))
+        element['img_url'] = ""
+        element['website'] = website_url[12:17]
+        element['main_name'] = ""
+
+
+        element = SimpleNamespace(**element)
+        modi_element = custom_detection(element, website_url[12:17])
+
+        new_element = ElementTable(
+            name=modi_element.name,
+            main_name=modi_element.main_name,
+            x_cod=modi_element.x_cod,
+            y_cod=modi_element.y_cod,
+            height=modi_element.height,
+            width=modi_element.width,
+            img_url=modi_element.img_url,
+            website=modi_element.website,
+            )
+        db.session.add(new_element)
+        db.session.commit()
+        return redirect(url_for("home_page"))
+
+    element = {
+        'name': "",
+        'x_cod': 0,
+        'y_cod': 0,
+        'height': 0,
+        'width': 0,
+        'img_url': "",
+        "website": website_url[12:12 + 5],
+        "main_name": ""
+    }
+    return render_template("edit_element.html", element=element, is_edit=False)
+
+
+@app.route("/web-control-api/<url>", methods=["GET", "POST"])
+def create_api(url):
+    table_data = ElementTable.query.filter_by(website=url).all()
+    json_element = []
+    if len(table_data) != 0:
+        for data in table_data:
+            json_element.append({
+                'name': data.name,
+                'main_name': data.main_name,
+                'x_cod': data.x_cod,
+                'y_cod': data.y_cod,
+                'height': data.height,
+                'width': data.width,
+                'img_url': data.img_url,
+                "website": data.website,
+            })
+    else:
+        json_element.append({
+            'error': 'This website data does not exist',
+            'statusCode': 404,
+        })
+
+    return json.dumps(json_element)
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
